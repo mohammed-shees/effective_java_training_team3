056c755ecb664fad4e0f8fb161f3bad0a1b2634b
mohammed-shees <49620014+mohammed-shees@users.noreply.github.com>
2021-10-29 09:15:24 +0530

    Delete Team - C/testapp12.finance6/src directory

--- Team - C/testapp12.finance6/src/tests/MyMaths.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-public class MyMaths {
-
-	public static int plus(int x,int y) {return x+y;}
-	public static int minus(int x,int y) {return x+y;}
-	public static int multiply(int x,int y) {return x*y;}
-	public static int divide(int x,int y) {return x/y;}
-}
